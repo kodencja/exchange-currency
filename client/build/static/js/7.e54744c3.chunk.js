0eab78301ddf5a45aa8b002fad31e6c7f0aabea4
kodencja <krzysztof.lalik.pl@gmail.com>
2021-05-01 12:33:30 +0200

    Add build

---
+++ client/build/static/js/7.e54744c3.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpclient=this.webpackJsonpclient||[]).push([[7],{70:function(n,c,i){}}]);
+//# sourceMappingURL=7.e54744c3.chunk.js.map
---
+++ client/build/static/js/8.2901e6d2.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpclient=this.webpackJsonpclient||[]).push([[8],{71:function(n,c,i){}}]);
+//# sourceMappingURL=8.2901e6d2.chunk.js.map
